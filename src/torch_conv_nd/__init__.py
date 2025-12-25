0bcdfb879de55f340d91b1201674843238e7244e
Felix Zimmermann <fzimmermann89@gmail.com>
2025-12-26 00:08:17 +0100

    add modules

--- src/torch_conv_nd/__init__.py
+++ src/torch_conv_nd/__init__.py
@@ -1,13 +1,14 @@
-"""ND Convolution for PyTorch."""
-
 __version__ = "0.1.0"
 
 from torch_conv_nd.functional import adjoint_pad_nd, conv_nd, pad_nd, pad_or_crop_to_size
+from torch_conv_nd.modules import ConvNd, ConvTransposeNd
 
 __all__ = [
     "adjoint_pad_nd",
     "conv_nd",
     "pad_nd",
     "pad_or_crop_to_size",
+    "ConvNd",
+    "ConvTransposeNd",
     "__version__",
 ]
